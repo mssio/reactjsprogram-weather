03e41f9d1fb5a474c270bc0b7690a4e3f9cb5d85
Mario Suharja <me@mario.sg>
2016-11-06 01:03:38 +0700

    Step 4
    - Add header style in Main container
    - Update render in Main container to use newly added style
    - Attach GetCityContainer inside Main container header using row direction

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,9 +1,19 @@
 var React = require('react');
 
+var GetCityContainer = require('../containers/GetCityContainer');
+
 var styles = {
   container: {
     width: '100%',
     height: '92%'
+  },
+  header :{
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: 'rgba(252, 90, 44, 0.89)',
+    color: '#fff',
+    padding: 5,
   }
 }
 
@@ -11,7 +21,10 @@ var Main = React.createClass({
   render: function () {
     return (
       <div style={styles.container}>
-        <h1>Main.js Header</h1>
+        <div style={styles.header}>
+          <h2 style={{margin: 0}}>Pawang Hujan</h2>
+          <GetCityContainer direction="row" />
+        </div>
         {this.props.children}
       </div>
     );
