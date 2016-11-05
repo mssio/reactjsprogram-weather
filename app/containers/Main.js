8ada7a738eecdf5e8edb08a34c2494e6e0757e67
Mario Suharja <me@mario.sg>
2016-11-05 23:36:01 +0700

    Step 2
    Feature:
    - Move HelloWorld to Main container
    - Setup routes with index to Main container
    - index.js now render routes instead of static HelloWorld

---
+++ app/containers/Main.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+function Main () {
+  return (
+    <div className="main-container">
+      <h1>Main.js Header</h1>
+    </div>
+  );
+}
+
+module.exports = Main;
--- app/index.js
+++ app/index.js
@@ -1,11 +1,9 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWorld = (
-  <h3>Hello World!</h3>
-);
+var routes = require('./config/routes');
 
 ReactDom.render(
-  HelloWorld,
+  routes,
   document.getElementById('app')
 );
