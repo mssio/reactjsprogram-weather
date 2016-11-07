a2983c6b5593eae69007bf7919788514375cd8f0
Mario Suharja <me@mario.sg>
2016-11-07 22:18:35 +0700

    Step 8
    - Add weather icons
    - Create dateHelpers to help format date
    - Update Forecast component to show awesome UI

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -2,20 +2,74 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 
 var Loading = require('./Loading');
+var formatDate = require('../utils/dateHelpers').formatDate;
+
+var styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    maxWidth: 1200,
+    margin: '50px auto'
+  },
+  dayContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    margin: 35
+  },
+  header: {
+    fontSize: 65,
+    color: '#333',
+    fontWeight: 100,
+    textAlign: 'center'
+  },
+  subheader: {
+    fontSize: 30,
+    color: '#333',
+    fontWeight: 100
+  },
+  weather: {
+    height: 130
+  }
+}
 
 function puke (obj) {
   return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
 }
 
+function DayItem (props) {
+  var date = formatDate(props.entry.dt);
+  var icon = props.entry.weather[0].icon;
+
+  return (
+    <div style={styles.dayContainer}>
+      <img style={styles.weather} src={'./app/images/weather-icons/' + icon + '.svg'} alt="Weather" />
+      <h2 style={styles.subheader}>{date}</h2>
+    </div>
+  );
+}
+
+function ForecastUI (props) {
+  return (
+    <div>
+      <h1 style={styles.header}>{props.city}</h1>
+      <div style={styles.container}>
+        {props.forecast.map(function (listItem) {
+          return (<DayItem key={listItem.dt} entry={listItem} />);
+        })}
+      </div>
+    </div>
+  );
+}
+
 function Forecast (props) {
   return props.isLoading === true
     ? <Loading />
-    : (
-      <div>
-        <h6>Forecast Component</h6>
-        {puke(props.weathersData)}
-      </div>
-    );
+    : (<ForecastUI city={props.weathersData.city.name} forecast={props.weathersData.list} />);
 }
 
 Forecast.propTypes = {
